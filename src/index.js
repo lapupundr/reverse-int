e79678e2922899ef63f84f872eb920b93e924544
Tatsiana Zatonets <koshka.tan@gmail.com>
2021-08-23 16:45:09 +0300

    reverse int ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    str = n.toString();
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+        result = `${str[i]}${result}`;
+    }
+    return +result.trim();
+
 }
